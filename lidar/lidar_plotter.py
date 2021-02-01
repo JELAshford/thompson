f409bdff6b945b3e2abd59497d0dfdb3c3d047a7
James Ashford <james.ashford@bnc.ox.ac.uk>
2021-02-01 23:33:37 +0000

    Updated plotter to (hopefully!) parse properly

--- lidar/lidar_plotter.py
+++ lidar/lidar_plotter.py
@@ -1,35 +1,30 @@
 # Plot the data from the Rpi, read through the "lidar_data" topic
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pylab as plt
+from matplotlib.animation import FuncAnimation
+
+import numpy as np
+
 import paho.mqtt.client as mqtt
 import json
 
 def lidar_callback(client, userdata, message):
+    global lidar_data
     decoded_message = json.loads(message.payload.decode())
-    print(decoded_message)
+    lidar_data = decoded_message
+    # print(decoded_message)
 
 def animate(i):
-    global plot_angle, plot_dist, temp_angle, temp_dist
+    global plot_angle, plot_dist, lidar_data
 
-    # Read in and unpack the most recent data
-    decoded = client.recv(1024).decode('utf-8')
-    new_data = json.loads(decoded)
+    if lidar_data[0] != plot_angle[-1][0]:
+        plot_angle.append(lidar_data[0])
+        plot_dist.append(lidar_data[1])
 
-    temp_angle.append((new_data[0]/360)*(np.pi*2))
-    temp_dist.append(new_data[1])
-
-    # Tell server data was recieved
-    client.send(json.dumps("Yep").encode('utf-8'))
-
-    # If it's been 40 steps, add to plot data
-    if i % 10 == 0:
-
-        plot_angle += temp_angle
-        plot_dist += temp_dist
-
-        temp_angle, temp_dist = [], []
-
-        # Re-create scatter
-        ax.plot(plot_angle, plot_dist, 'r.')
-        ax.set_theta_direction(-1)
+    # Re-create scatter
+    ax.plot(plot_angle, plot_dist, 'r.')
+    ax.set_theta_direction(-1)
 
 # Connect to the Brain client
 broker_url, broker_port = "192.168.10.103", 1883
@@ -40,4 +35,15 @@ client.connect(broker_url, broker_port)
 client.subscribe("lidar_data", qos=0)
 client.message_callback_add("lidar_data", lidar_callback)
 
-client.loop_forever()
+# Storage for lidar data
+lidar_data = []
+
+# Wrap the plotting in the client loop
+client.loop_start()
+
+plot_angle, plot_dist = [], []
+ax = plt.subplot(111, projection='polar')
+ani = FuncAnimation(plt.gcf(), animate, interval=1)
+plt.show()
+
+client.loop_stop()
