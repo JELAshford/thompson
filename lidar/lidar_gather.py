daf2e7624f4e39e5506d2799b88d476e5c578d13
James Ashford <james.ashford@bnc.ox.ac.uk>
2021-02-02 11:49:18 +0000

    Shifed around to allow multi-scan...hopefully!

--- lidar/lidar_gather.py
+++ lidar/lidar_gather.py
@@ -13,10 +13,10 @@ def run_scan(client, userdata, message):
         MAX_SAMPLES = request["MAX_SAMPLES"]
         SLEEP_TIME = request["SLEEP_TIME"]
     # Start the lidar scan
-    lidar = PyRPlidar()
+    # lidar = PyRPlidar()
     lidar.connect(port="/dev/ttyUSB0", baudrate=115200, timeout=3)
-    lidar.set_motor_pwm(500)
-    time.sleep(SLEEP_TIME)
+    # lidar.set_motor_pwm(500)
+    # time.sleep(SLEEP_TIME)
 
     # Storage for scan data
     SAMPLE_BATCH = []
@@ -33,18 +33,18 @@ def run_scan(client, userdata, message):
         if len(SAMPLE_BATCH) > MAX_SAMPLES:
             break
     
-    lidar.stop()
-    lidar.connect(port="/dev/ttyUSB0", baudrate=115200, timeout=3)
-    scan_generator = lidar.start_scan()
-    for scan in scan_generator():
+    # lidar.stop()
+    # lidar.connect(port="/dev/ttyUSB0", baudrate=115200, timeout=3)
+    # scan_generator = lidar.start_scan()
+    # for scan in scan_generator():
 
-        # If scan meets quality standard, add to BATCH
-        if scan.quality > 10:
-            SAMPLE_BATCH.append([scan.angle, scan.distance])
+    #     # If scan meets quality standard, add to BATCH
+    #     if scan.quality > 10:
+    #         SAMPLE_BATCH.append([scan.angle, scan.distance])
 
-        # Break if reached the maximum number of samples
-        if len(SAMPLE_BATCH) > MAX_SAMPLES:
-            break
+    #     # Break if reached the maximum number of samples
+    #     if len(SAMPLE_BATCH) > MAX_SAMPLES:
+    #         break
 
 
     # Package with json and send
@@ -52,9 +52,9 @@ def run_scan(client, userdata, message):
     client.publish(topic="lidar_batch", payload=message, qos=0, retain=False)
     
     # Stop the lidar
-    lidar.set_motor_pwm(0)
+    # lidar.set_motor_pwm(0)
     lidar.stop()
-    lidar.disconnect()
+    # lidar.disconnect()
 
 
 # Connect to the Brain client
@@ -62,8 +62,18 @@ broker_url, broker_port = "192.168.10.100", 1883
 client = mqtt.Client()
 client.connect(broker_url, broker_port)
 
+# Start the lidar scan
+lidar = PyRPlidar()
+lidar.set_motor_pwm(500)
+
 # Subscribe to request topic
 client.subscribe("lidar_request", qos=0)
 client.message_callback_add("lidar_request", run_scan)
 
-client.loop_forever()
+try:
+    client.loop_forever()
+finally:
+    # Stop the lidar
+    lidar.set_motor_pwm(0)
+    lidar.stop()
+    lidar.disconnect()
