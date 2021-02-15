5a7c95a183467e996bd18d8286a1dc0a8df0ccf4
James Ashford <james.ashford@bnc.ox.ac.uk>
2021-02-15 22:00:29 +0000

    Push higher than before

--- lidar/lidar_gather.py
+++ lidar/lidar_gather.py
@@ -52,7 +52,7 @@ client.connect(broker_url, broker_port)
 # Create the lidar object to connect to
 lidar = PyRPlidar()
 lidar.connect(port="/dev/ttyUSB0", baudrate=115200, timeout=3)
-lidar.set_motor_pwm(300)
+lidar.set_motor_pwm(1000)
 lidar.stop()
 
 # Subscribe to request topic
