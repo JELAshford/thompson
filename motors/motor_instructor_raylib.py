b3aba971a95ebc331d86443308960126f47a2383
James Ashford <james.ashford@bnc.ox.ac.uk>
2023-12-06 19:26:03 +0000

    Modified to allow multiple instructions without command overflow

--- motors/motor_instructor_raylib.py
+++ motors/motor_instructor_raylib.py
@@ -24,33 +24,36 @@ while not pr.window_should_close():
     pr.clear_background(pr.BLACK)
     pr.draw_text("Interface! Yay!", 0, 10, 12, pr.RED)
 
-    command = None
+    commands = []
     if pr.is_key_down(pr.KeyboardKey.KEY_UP):
-        command = [2, [1, MOVE_POWER, 0.1]]
+        commands.append([2, [1, MOVE_POWER, 0.1]])
 
     if pr.is_key_down(pr.KeyboardKey.KEY_DOWN):
-        command = [2, [-1, MOVE_POWER, 0.1]]
+        commands.append([2, [-1, MOVE_POWER, 0.1]])
 
     if pr.is_key_down(pr.KeyboardKey.KEY_LEFT):
-        command = [1, [-TURN_STEP]]
+        commands.append([1, [-TURN_STEP]])
 
     if pr.is_key_down(pr.KeyboardKey.KEY_RIGHT):
-        command = [1, [TURN_STEP]]
+        commands.append([1, [TURN_STEP]])
 
     # Send command if provided
-    if command is not None:
-        if pr.get_time() - command_time > 0.1:
+    if len(commands) != 0:
+        if pr.get_time() - command_time > (0.1 * len(commands)):
             command_time = pr.get_time()
-            command_text = str(command)
-            message = json.dumps(command).encode("utf-8")
-            client.publish(topic="motor_request", payload=message, qos=0, retain=False)
+            command_text = ",".join([*map(str, commands)])
+            for command in commands:
+                message = json.dumps(command).encode("utf-8")
+                client.publish(
+                    topic="motor_request", payload=message, qos=0, retain=False
+                )
 
     # Draw command text to window, persist for 2 seconds unless overwritten
     time_since_last_command = pr.get_time() - command_time
     command_text = (
         command_text
         if time_since_last_command < command_text_lifetime
-        else str(command)
+        else ",".join([*map(str, commands)])
     )
     pr.draw_text(f"Command: {command_text}", 0, 25, 15, pr.WHITE)
     pr.end_drawing()
